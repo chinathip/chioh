033bd94bbd356b83f353837ebcc24ef5a63020fe
Chinathip pondumpai <cc.chinathip@gmail.com>
2019-05-15 16:36:13 +0700

    update file env

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'json'
 require 'line/bot'
 require 'dotenv/load'
 
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
 get '/' do
     'Hello world!'
   end
