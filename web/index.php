2a44eac1fc4d7ac896723c1e519409b454b7c38c
Chinathip pondumpai <cc.chinathip@gmail.com>
2019-05-16 23:12:09 +0700

    test31

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'Dy97GGOwvP0GXu9Rfv69qjzk5srIDCRfOV9J1kEJF9F+4PUsxpyko5PPwmqXgwUcH9Ry+SwmpasJFyVZCjOCBOpq0LA0Lgeb+pr0I9J1ics7ixZERyn2gNOOKjXlGCl1teKxgAPGwDatiXdifCUfXQdB04t89/1O/w1cDnyilFU=
 ';
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
